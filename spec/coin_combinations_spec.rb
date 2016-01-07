03ee83634d1cd8c34703631db797d0c22c48c80c
Patrick Sullivan and Riley Starnes <riley@>
2016-01-07 09:46:17 -0800

    pass third spec for plural pennies

--- spec/coin_combinations_spec.rb
+++ spec/coin_combinations_spec.rb
@@ -15,7 +15,7 @@ describe('Float#coin_combinations') do
     expect((0.20).coin_combinations).to(eq("2 dimes"))
     expect((2.00).coin_combinations).to(eq("2 dollars"))
   end
-
+  
   it('replaces plural pennys with with pennies') do
     expect((0.02).coin_combinations).to(eq("2 pennies"))
   end
