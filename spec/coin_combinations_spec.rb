b8da7822349e64122bd51e8372baa7233979b5b3
Patrick Sullivan and Riley Starnes <patrick@>
2016-01-07 09:26:38 -0800

    update first test to account for each type of coin

--- spec/coin_combinations_spec.rb
+++ spec/coin_combinations_spec.rb
@@ -2,7 +2,11 @@ require('rspec')
 require('coin_combinations.rb')
 
 describe('Float#coin_combinations') do
-  it ('return 1 quarter for input of 0.25') do
+  it ('return correct coin combination for input') do
     expect((0.25).coin_combinations).to(eq("1 quarter"))
+    expect((1.00).coin_combinations).to(eq("1 dollar"))
+    expect((0.05).coin_combinations).to(eq("1 nickel"))
+    expect((0.01).coin_combinations).to(eq("1 penny"))
+    expect((0.10).coin_combinations).to(eq("1 dime"))
   end
 end
