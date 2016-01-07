48052144597452fbdab7bc9a90c208aca34d8d42
Patrick Sullivan and Riley Starnes <riley@>
2016-01-07 11:17:24 -0800

    added styling

--- app.rb
+++ app.rb
@@ -3,10 +3,13 @@ require('sinatra/reloader')
 require('./lib/coin_combinations')
 
 get('/') do
+  File.read(File.join('public', 'css/styles.css'))
+  @title = "Coin Combinations!"
   erb(:index)
 end
 
 get('/result') do
+  @title = "Results"
   @change = params.fetch('change').to_f.coin_combinations
   erb(:result)
 end
